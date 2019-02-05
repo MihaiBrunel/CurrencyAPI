1b38a00b0e50231c72f15073f46c005bede19add
MihaiBrunel <1700441@brunel.ac.uk>
2019-02-05 01:50:23 +0000

    First version of the working API, it has the ability to compare any currency to USD

---
+++ app/src/main/java/com/example/currencyapi/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.currencyapi;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Button button = (Button) findViewById(R.id.button);
+        final TextView textView = (TextView) findViewById(R.id.text);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                try {
+                    Personal currency = new Personal("USD");
+                    Double d = Personal.getData("GBP");
+
+                    textView.setText(d.toString());
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+                } catch (JSONException e) {
+
+                    e.printStackTrace();
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+    }
+}
